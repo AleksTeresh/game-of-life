fef868bc0c47a81af42c437afd17d1de174b27b6
Aleksandr Tereshchenko <aleksandr.tereshchenko@reaktor.com>
2019-07-01 22:16:47 +0300

    Random state generation on keyDown

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
-import React from 'react'
-import logo from './logo.svg'
+import React, { useState, useEffect } from 'react'
 import './App.css'
-import Board from './components/Board';
+import Board from './components/Board'
 
 function App() {
+  const [gens, setGens] = useState([Array(100).fill(0).map(row => (Array(100).fill(0).map(cell => ({ alive: (Math.random() < 0.5) }))))])
+
+  useEffect(() => {
+    const evenHandler = (e: any) => {
+      if (e.keyCode === 13) { // if Enter
+        setGens([
+          ...gens,
+          Array(100).fill(0).map(row => (Array(100).fill(0).map(cell => ({ alive: (Math.random() < 0.5) }))))
+        ])
+      }
+    }
+    window.addEventListener("keypress", evenHandler, false)
+
+    return () => {
+      window.removeEventListener("keypress", evenHandler, false)
+    }
+  }, [])
+
   return (
     <div className="App">
       <Board cells={
-        [
-          [{ alive: false },{ alive: true },{ alive: false }], [{ alive: false },{ alive: false },{ alive: true }]
-        ]
+        gens.length ? gens[gens.length - 1] : []
       } />
     </div>
   );
