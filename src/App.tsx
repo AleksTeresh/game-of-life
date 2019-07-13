45495b9ae8dcbae8ab4417f9fe6c58ccbae464b9
Aleksandr Tereshchenko <aleksandr.tereshchenko@reaktor.com>
2019-07-14 00:27:47 +0300

    Use mutation queries to go back and forth between generations

--- src/App.tsx
+++ src/App.tsx
@@ -12,16 +12,39 @@ const GENERATIONS_QUERY = gql`
   }
 `
 
-export default function App() {
+const NEXT_GEN_MUTATION = gql`
+  mutation NextGeneration {
+    nextGeneration {
+      alive
+    }
+  }
+`
+
+const PREV_GEN_MUTATION = gql`
+  mutation PrevGeneration {
+    prevGeneration {
+      alive
+    }
+  }
+`
+
+export default function App({ client }: { client: any }) {
   const evenHandler = (e: any) => {
     if (e.keyCode === 13) { // if Enter
-      
+      client.mutate({
+        mutation: NEXT_GEN_MUTATION,
+        refetchQueries: () => [{ query: GENERATIONS_QUERY }]
+      })
     } else if (e.keyCode === 32) { // space
-      
+      e.preventDefault()
+      client.mutate({
+        mutation: PREV_GEN_MUTATION,
+        refetchQueries: () => [{ query: GENERATIONS_QUERY }]
+      })
     }
   }
 
-  useEffect(() => {  
+  useEffect(() => {
     window.addEventListener("keypress", evenHandler, false)
     return () => {
       window.removeEventListener("keypress", evenHandler, false)
