5bf93f0edd75dacd444928e5383095c0b4354f93
Aleksandr Tereshchenko <aleksandr.tereshchenko@reaktor.com>
2019-07-14 15:17:17 +0300

    Refactor, add react-route

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,15 @@
-import React, { useEffect } from 'react'
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+
 import './App.css'
-import Board from './components/Board'
-import gql from 'graphql-tag'
-import { Query } from 'react-apollo'
-
-const GENERATIONS_QUERY = gql`
-  {
-    currGeneration {
-      alive
-    }
-  }
-`
-
-const NEXT_GEN_MUTATION = gql`
-  mutation NextGeneration {
-    nextGeneration {
-      alive
-    }
-  }
-`
-
-const PREV_GEN_MUTATION = gql`
-  mutation PrevGeneration {
-    prevGeneration {
-      alive
-    }
-  }
-`
+import Game from './components/Game'
 
 export default function App({ client }: { client: any }) {
-  const evenHandler = (e: any) => {
-    if (e.keyCode === 13) { // if Enter
-      client.mutate({
-        mutation: NEXT_GEN_MUTATION,
-        refetchQueries: () => [{ query: GENERATIONS_QUERY }]
-      })
-    } else if (e.keyCode === 32) { // space
-      e.preventDefault()
-      client.mutate({
-        mutation: PREV_GEN_MUTATION,
-        refetchQueries: () => [{ query: GENERATIONS_QUERY }]
-      })
-    }
-  }
-
-  useEffect(() => {
-    window.addEventListener("keypress", evenHandler, false)
-    return () => {
-      window.removeEventListener("keypress", evenHandler, false)
-    }
-  })
-
   return (
     <div className="App">
-      <Query query={GENERATIONS_QUERY}>
-      {({ loading, error, data }: { loading: boolean, error?: any, data: any }) => {
-        if (loading) return <div>Fetching</div>
-        if (error) {
-          console.error(error)
-          return <div>Error</div>
-        }
-
-        const currGeneration = data.currGeneration
-        return (
-          <Board cells={currGeneration} />
-        )
-      }}
-      </Query>      
+      <Switch>
+        <Route exact path="/" component={() => <Game client={client} />} />
+      </Switch>
     </div>
-  );
+  )
 }
---
+++ src/components/Game.tsx
@@ -0,0 +1,72 @@
+import React, { useEffect } from 'react'
+import gql from 'graphql-tag'
+import { Query } from 'react-apollo'
+
+import Board from './Board'
+
+const GENERATIONS_QUERY = gql`
+  {
+    currGeneration {
+      alive
+    }
+  }
+`
+
+const NEXT_GEN_MUTATION = gql`
+  mutation NextGeneration {
+    nextGeneration {
+      alive
+    }
+  }
+`
+
+const PREV_GEN_MUTATION = gql`
+  mutation PrevGeneration {
+    prevGeneration {
+      alive
+    }
+  }
+`
+
+export default function Game({ client }: { client: any }) {
+  const evenHandler = (e: any) => {
+    if (e.keyCode === 13) { // if Enter
+      client.mutate({
+        mutation: NEXT_GEN_MUTATION,
+        refetchQueries: () => [{ query: GENERATIONS_QUERY }]
+      })
+    } else if (e.keyCode === 32) { // space
+      e.preventDefault()
+      client.mutate({
+        mutation: PREV_GEN_MUTATION,
+        refetchQueries: () => [{ query: GENERATIONS_QUERY }]
+      })
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("keypress", evenHandler, false)
+    return () => {
+      window.removeEventListener("keypress", evenHandler, false)
+    }
+  })
+
+  return (
+    <div>
+      <Query query={GENERATIONS_QUERY}>
+      {({ loading, error, data }: { loading: boolean, error?: any, data: any }) => {
+        if (loading) return <div>Fetching</div>
+        if (error) {
+          console.error(error)
+          return <div>Error</div>
+        }
+
+        const currGeneration = data.currGeneration
+        return (
+          <Board cells={currGeneration} />
+        )
+      }}
+      </Query>      
+    </div>
+  );
+}
