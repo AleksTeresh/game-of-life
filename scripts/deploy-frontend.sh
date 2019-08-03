c825496a355d00fda2b6714ad4c2828b60a515c7
Aleksandr Tereshchenko <aleksandr.tereshchenko@reaktor.com>
2019-08-03 23:34:14 +0300

    Add deployment scripts for AWS ECR

---
+++ scripts/deploy-frontend.sh
@@ -0,0 +1,4 @@
+$(aws ecr get-login --no-include-email --region us-east-1)
+docker build -t game-of-life-frontend ../web
+docker tag game-of-life-frontend:latest 962168178617.dkr.ecr.us-east-1.amazonaws.com/game-of-life-frontend:latest
+docker push 962168178617.dkr.ecr.us-east-1.amazonaws.com/game-of-life-frontend:latest
