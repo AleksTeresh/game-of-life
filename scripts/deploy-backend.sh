f17478accdc8d1d0a2e36aeb4a1fc27408139c5e
Aleksandr Tereshchenko <aleksandr.tereshchenko@reaktor.com>
2019-08-04 19:34:57 +0300

    Add --no-cache flag to docker build steps of deployment scripts

--- scripts/deploy-backend.sh
+++ scripts/deploy-backend.sh
@@ -1,4 +1,4 @@
 $(aws ecr get-login --no-include-email --region us-east-1)
-docker build -t game-of-life-backend ../backend
+docker build --no-cache -t game-of-life-backend ../backend
 docker tag game-of-life-backend:latest 962168178617.dkr.ecr.us-east-1.amazonaws.com/game-of-life-backend:latest
 docker push 962168178617.dkr.ecr.us-east-1.amazonaws.com/game-of-life-backend:latest
